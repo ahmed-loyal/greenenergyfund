44314dd0582c5b8f3b8efd592ae9c8a9de2004dc
ahmedloyal <awojobiahmed71@gmail.com>
2024-05-11 22:12:02 +0100

    corrected controller

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const adminController = require('../controllers/admincontroller');
+const adminController = require('../controllers/adminController');
 const { requireAuth } = require('../middleware/authMiddleware');
 
 const router = Router();
